1d0fab1c2e0f718262d91f88e83871a74325ca38
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-01-11 21:57:05 +0500

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+#--Previos Route [ Route 7 ]---------------------------------
+#Prefix            | article
+#Verb              | GET
+#URI               | /articles/:id(.:format)
+#Controller#Action | articles#show
+#------------------------------------------------------------
+  resources :articles, only: [:show]
+#--New Route [ Route 3 ]-------------------------------------
+#Prefix            | article
+#Verb              | GET
+#URI               | /articles/:id(.:format)
+#Controller#Action | articles#show
+#------------------------------------------------------------
 end
