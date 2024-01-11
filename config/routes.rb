bfff801d45f30dc624a58440e2252ebe3f3105ac
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-01-11 22:33:55 +0500

    Add articles Listing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 #URI               | /articles/:id(.:format)
 #Controller#Action | articles#show
 #------------------------------------------------------------
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 #--New Route [ Route 3 ]-------------------------------------
 #Prefix            | article
 #Verb              | GET
