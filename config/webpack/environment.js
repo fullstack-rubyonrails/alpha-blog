40c8e23dbeb086910cd3c86926f6daf7ee9a9fcd
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-01-18 22:30:52 +0500

    Final Code with cleaned-Up Layouts

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+//Enable JS functionality here
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
