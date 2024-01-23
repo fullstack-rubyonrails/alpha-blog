f53e96204d35d4d2f2a3369124408bfe2d695b19
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-01-23 23:05:40 +0500

    Add one to many  associations between user and articles from backend

---
+++ db/Reset Counter.sql
@@ -0,0 +1,15 @@
+-- UPDATE 'sqlite_sequence'
+-- SET 'seq' = (SELECT max(id) from articles)
+-- WHERE name= 'articles';
+
+-- UPDATE 'sqlite_sequence'
+-- SET '0' = (SELECT MAX('id') FROM 'articles')
+-- WHERE `name` = 'articles';
+
+-- UPDATE 'sqlite_sequence' SET 'seq' = 0 WHERE 'name' = 'table_name';
+
+-- SELECT * FROM `sqlite_sequence`;
+
+-- SELECT max(id) from articles;
+
+-- SELECT * from articles;
