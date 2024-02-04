374369ac6b9044fb256a25290c066b241bc6c32a
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-02-04 15:11:31 +0500

    Navigations for user session name, login-logout link and helper method modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -13,10 +13,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
+
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -28,7 +27,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha-Blog #{@user.username}, you  have successully signed-up"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha-Blog #{@user.username.upcase}, you  have successully signed-up"
       redirect_to articles_path
     else
       render 'new'
@@ -37,5 +37,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password_digest)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
