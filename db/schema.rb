69ec44d868ae908bea68db66e2fc18a60de3a019
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-02-11 22:34:56 +0500

    Add admin user field in user table, type is boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_24_174207) do
+ActiveRecord::Schema.define(version: 2024_02_11_172559) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2024_01_24_174207) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
